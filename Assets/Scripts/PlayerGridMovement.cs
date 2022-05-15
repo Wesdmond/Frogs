39b728a3512dbb801a0a7a08c5e27f61b820db8d
Wesdmond <SHto2004@ya.ru>
2022-05-15 21:59:21 +0500

    change frog animator structure

--- Assets/Scripts/PlayerGridMovement.cs
+++ Assets/Scripts/PlayerGridMovement.cs
@@ -15,10 +15,14 @@ public class PlayerGridMovement : MonoBehaviour
     [SerializeField]
     private LayerMask whatStopsMovement;
 
+    private Animator animator;
+    private string activeDirection = "Down";
+
 
     void Start()
     {
         _movePoint.parent = null;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,11 +31,28 @@ public class PlayerGridMovement : MonoBehaviour
 
         if (Vector3.Distance(transform.position, _movePoint.position) == 0f)
         {
+            animator.SetBool("Jumping", false);
+
             float _x = _playerInput.GetMovement().x;
             float _y = _playerInput.GetMovement().y;
 
             if (Mathf.Abs(_x) == 1)
             {
+                // There need to play sound
+                animator.SetBool("Jumping", true);
+                if (_x > 0)
+                {
+                    animator.SetBool(activeDirection, false);
+                    activeDirection = "Right";
+                    animator.SetBool(activeDirection, true);
+                }
+                else
+                {
+                    animator.SetBool(activeDirection, false);
+                    activeDirection = "Left";
+                    animator.SetBool(activeDirection, true);
+                }
+
                 if (!Physics2D.OverlapCircle(transform.position + new Vector3(_x, 0, 0), .2f, whatStopsMovement))
                 {
                     _movePoint.position = transform.position + new Vector3(_x, 0, 0);
@@ -39,6 +60,21 @@ public class PlayerGridMovement : MonoBehaviour
             }
             else if (Mathf.Abs(_y) == 1)
             {
+                // There need to play sound
+                animator.SetBool("Jumping", true);
+                if (_y > 0)
+                {
+                    animator.SetBool(activeDirection, false);
+                    activeDirection = "Up";
+                    animator.SetBool(activeDirection, true);
+                }
+                else
+                {
+                    animator.SetBool(activeDirection, false);
+                    activeDirection = "Down";
+                    animator.SetBool(activeDirection, true);
+                }
+
                 if (!Physics2D.OverlapCircle(transform.position + new Vector3(0, _y, 0), .2f, whatStopsMovement))
                 {
                     _movePoint.position = transform.position + new Vector3(0, _y, 0);
