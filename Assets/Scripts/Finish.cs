45edabe160f36a46cffc5572c211e45497a3ed3d
Wesdmond <SHto2004@ya.ru>
2022-05-28 21:22:22 +0500

    fix finish script

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -8,8 +8,6 @@ public class Finish : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
-            if ((SceneManager.GetActiveScene().buildIndex) + 1 < (SceneManager.sceneCount - 1))
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            else print("This is the last level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
