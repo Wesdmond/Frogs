a043978bc260d310b06ad11877903eeedd46b238
Ilya Shabanov <shio2001@ya.ru>
2022-06-13 17:22:51 +0500

    partly done work

---
+++ Assets/Scripts/Player/FrogConstants.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FrogConstants
+{
+    public struct FrogAnimations
+    {
+        public static readonly string moveRight = "Right";
+        public static readonly string moveLeft = "Left";
+        public static readonly string moveUp = "Up";
+        public static readonly string moveDown = "Down";
+    }
+
+    public struct FrogMaps
+    {
+        public static readonly string FrogDefaultMap = "Frog";
+        public static readonly string FrogShootingMap = "FrogShootingMode";
+    }
+
+}
--- Assets/Scripts/Player/PlayerGridMovement.cs
+++ Assets/Scripts/Player/PlayerGridMovement.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerGridMovement : MonoBehaviour
 {
+    [Range(0f, 1f)]
     [SerializeField] private float _moveSpeed = 5;
     [SerializeField] private int _moveDistance = 500;
     [SerializeField] private Transform _movePoint;
-    [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private LayerMask _whatStopsMovement;
     [SerializeField] private LayerMask _drownLayer;
     [SerializeField] private LayerMask _itemLayer;
+    [SerializeField]
+    private PlayerInput frogInput;
 
     [Header("MoveCounter")]
     [SerializeField]
@@ -28,12 +32,13 @@ public class PlayerGridMovement : MonoBehaviour
     [SerializeField]
     private SpriteRenderer _tongueSpriteRenderer;
 
-
-    private string _activeDirection = "Down";
     private float _time = 0;
     private float _basicSpeed;
     private int _tongueSortingOrder = 11;
 
+    private bool isMoving = false;
+
+    private FrogStates currentState = FrogStates.Idle;
 
     void Start()
     {
@@ -42,196 +47,264 @@ public class PlayerGridMovement : MonoBehaviour
         _animator.speed = _moveSpeed / 5;
     }
 
-    void Update()
+    public void Move(CallbackContext context)
     {
-
-        if (_time <= 0)
+        if (isMoving)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            _time -= _moveSpeed * Time.deltaTime;
+            return;
         }
 
-        // Moving mode
-        if (!_playerInput.GetShootingMode() || Vector3.Distance(transform.position, _movePoint.position) != 0f)
+        bool canMove = currentState == FrogStates.Idle || currentState == FrogStates.Moving;
+
+        if (canMove)
         {
-
-            _animator.SetBool("Shooting", false);
-
-            // Legacy code
-            /*if (_time <= 0)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
-            }
-            else
-            {
-                _time -= _moveSpeed * Time.deltaTime;
-            }*/
-
-            if (Vector3.Distance(transform.position, _movePoint.position) == 0.0f)
-            {
-                _animator.SetBool("Jumping", false);
-
-                float _x = _playerInput.GetMovement().x;
-                float _y = _playerInput.GetMovement().y;
-
-                if (_y < 0) _tongueSortingOrder = 11;
-                else if (_y != 0 || _x != 0) _tongueSortingOrder = 9;
-
-                if (Mathf.Abs(_x) == 1)
-                {
-                    // There need to play sound
-                    // AudioManager.Instance.Play("Jump");
-
-                    _animator.SetBool("Jumping", true);
-                    _time = 1;
-                    if (_x > 0)
-                    {
-                        _animator.SetBool(_activeDirection, false);
-                        _activeDirection = "Right";
-                        _animator.SetBool(_activeDirection, true);
-                        _tongueTransform.rotation = Quaternion.Euler(0, 0, 90);
-                    }
-                    else
-                    {
-                        _animator.SetBool(_activeDirection, false);
-                        _activeDirection = "Left";
-                        _animator.SetBool(_activeDirection, true);
-                        _tongueTransform.rotation = Quaternion.Euler(0, 0, 270);
-                    }
-
-                    // Check if there drowning object
-                    if (Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _drownLayer) != null && 
-                        Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _itemLayer) == null)
-                    {
-                        _movePoint.position = transform.position + new Vector3(_x * _moveDistance, 0, 0);
-                    }
-
-                    // Check if there no tile collider
-                    else if (Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _whatStopsMovement) == null)
-                    {
-                        _movePoint.position = transform.position + new Vector3(_x * _moveDistance, 0, 0);
-                        if (enableMoveCounter) _moveCounter.Move();
-                    }
-
-                }
-                else if (Mathf.Abs(_y) == 1)
-                {
-                    // There need to play sound
-                    // AudioManager.Instance.Play("Jump");
-
-                    _animator.SetBool("Jumping", true);
-                    _time = 1;
-                    if (_y > 0)
-                    {
-                        _animator.SetBool(_activeDirection, false);
-                        _activeDirection = "Up";
-                        _animator.SetBool(_activeDirection, true);
-                        _tongueTransform.rotation = Quaternion.Euler(0, 0, 180);
-                    }
-                    else
-                    {
-                        _animator.SetBool(_activeDirection, false);
-                        _activeDirection = "Down";
-                        _animator.SetBool(_activeDirection, true);
-                        _tongueTransform.rotation = Quaternion.Euler(0, 0, 0);
-                    }
-
-                    // Check if there drowning object
-                    if (Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _drownLayer) != null &&
-                        Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _itemLayer) == null)
-                    {
-                        _movePoint.position = transform.position + new Vector3(0, _y * _moveDistance, 0);
-                    }
-
-                    // Check if there no tile collider
-                    else if (Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _whatStopsMovement) == null)
-                    {
-                        _movePoint.position = transform.position + new Vector3(0, _y * _moveDistance, 0);
-                        if (enableMoveCounter) _moveCounter.Move();
-                    }
-
-                }
-            }
-        }
-
-        // Shooting mode
-        else
-        {
-            _animator.SetBool("Shooting", true);
-            float _x = _playerInput.GetMovement().x;
-            float _y = _playerInput.GetMovement().y;
-            if (_y < 0) _tongueSortingOrder = 11;
-            else if (_y != 0 || _x != 0) _tongueSortingOrder = 9;
-
-            if (_playerInput.GetShoot() && !_tongue.IsRunning)
-            {
-                if (enableMoveCounter) _moveCounter.Move();
-
-                _tongueTransform.gameObject.SetActive(true);
-                _tongueSpriteRenderer.sortingOrder = _tongueSortingOrder;
-                _tongue.StartShootTongue();
-
-                _playerInput.SetFreeze(true);
-                _playerInput.SetShoot(false);
-            }
-
-            _tongueTransform.gameObject.SetActive(_tongue.IsRunning);
-            _playerInput.SetFreeze(_tongue.IsRunning);
-            if (_tongue.IsRunning)
-            {
-                _moveSpeed = _tongue.Speed;
-            }
-            else
-            {
-                _moveSpeed = _basicSpeed;
-            }
-
-
-            if (Mathf.Abs(_x) == 1)
-            {
-                // There need to play shooting sound
-                // AudioManager.Instance.Play("shoot")
-
-                if (_x > 0)
-                {
-                    _animator.SetBool(_activeDirection, false);
-                    _activeDirection = "Right";
-                    _animator.SetBool(_activeDirection, true);
-                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 90);
-                }
-                else
-                {
-                    _animator.SetBool(_activeDirection, false);
-                    _activeDirection = "Left";
-                    _animator.SetBool(_activeDirection, true);
-                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 270);
-                }
-            }
-
-            else if (Mathf.Abs(_y) == 1)
-            {
-                // There need to play shooting sound
-                // AudioManager.Instance.Play("Shoot");
-
-                if (_y > 0)
-                {
-                    _animator.SetBool(_activeDirection, false);
-                    _activeDirection = "Up";
-                    _animator.SetBool(_activeDirection, true);
-                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 180);
-                }
-                else
-                {
-                    _animator.SetBool(_activeDirection, false);
-                    _activeDirection = "Down";
-                    _animator.SetBool(_activeDirection, true);
-                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-            }
+            Vector2 inputVector = context.ReadValue<Vector2>();
             
+            StartCoroutine(MoveCoroutine(inputVector));
+
         }
+    }
+    private IEnumerator MoveCoroutine(Vector2 inputVector)
+    {
+        isMoving = true;
+
+        int _x = Mathf.RoundToInt(inputVector.x);
+        int _y = Mathf.RoundToInt(inputVector.y);
+
+        if (_x != 0)
+        {
+            _movePoint.position = _movePoint.position + new Vector3(_moveDistance * _x, 0, 0);
+            _tongueTransform.rotation = Quaternion.Euler(0, 0, 90 * _x);
+        }  
+        else if (_y != 0)
+        {
+            _movePoint.position = _movePoint.position + new Vector3(0, _moveDistance * _y, 0);
+            _tongueTransform.rotation = Quaternion.Euler(0, 0, 90 + 90 * _y);
+        }
+
+        float _speed = _moveDistance * _moveSpeed;
+        for (int i = 0; i < 1/_moveSpeed; i++)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _speed);
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+
+    public void ChangeShootingMode()
+    {
+        if (currentState == FrogStates.Idle)
+        {
+            frogInput.SwitchCurrentActionMap(FrogConstants.FrogMaps.FrogShootingMap);
+            currentState = FrogStates.ShootingMode;
+        }
+
+        if (currentState == FrogStates.ShootingMode)
+        {
+            frogInput.SwitchCurrentActionMap(FrogConstants.FrogMaps.FrogDefaultMap);
+            currentState = FrogStates.Idle;
+        }
+    }
+
+    public void Shoot()
+    {
+        bool canShoot = currentState == FrogStates.ShootingMode;
+
+        if (canShoot)
+        {
+            // Placeholder
+            Debug.LogWarning("Shooting is not implemented yet!");
+        }
+    }
+
+    //void Update()
+    //{
+
+    //    if (_time <= 0)
+    //    {
+    //        transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
+    //    }
+    //    else
+    //    {
+    //        _time -= _moveSpeed * Time.deltaTime;
+    //    }
+
+    //    // Moving mode
+    //    if (!_playerInput.GetShootingMode() || Vector3.Distance(transform.position, _movePoint.position) != 0f)
+    //    {
+    //        _animator.SetBool("Shooting", false);
+
+    //        if (Mathf.RoundToInt(Vector3.Distance(transform.position, _movePoint.position)) == 0)
+    //        {
+    //            _animator.SetBool("Jumping", false);
+
+    //            float _x = _playerInput.GetMovement().x;
+    //            float _y = _playerInput.GetMovement().y;
+
+    //            if (_y < 0) _tongueSortingOrder = 11;
+    //            else if (_y != 0 || _x != 0) _tongueSortingOrder = 9;
+
+    //            if (Mathf.Abs(_x) == 1)
+    //            {
+    //                // There need to play sound
+    //                // AudioManager.Instance.Play("Jump");
+
+    //                _animator.SetBool("Jumping", true);
+    //                _time = 1;
+    //                if (_x > 0)
+    //                {
+    //                    _animator.SetBool(_activeDirection, false);
+    //                    _activeDirection = "Right";
+    //                    _animator.SetBool(_activeDirection, true);
+    //                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 90);
+    //                }
+    //                else
+    //                {
+    //                    _animator.SetBool(_activeDirection, false);
+    //                    _activeDirection = "Left";
+    //                    _animator.SetBool(_activeDirection, true);
+    //                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 270);
+    //                }
+
+    //                // Check if there drowning object
+    //                if (Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _drownLayer) != null && 
+    //                    Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _itemLayer) == null)
+    //                {
+    //                    _movePoint.position = transform.position + new Vector3(_x * _moveDistance, 0, 0);
+    //                }
+
+    //                // Check if there no tile collider
+    //                else if (Physics2D.OverlapCircle(transform.position + new Vector3(_x * _moveDistance, 0, 0), .2f, _whatStopsMovement) == null)
+    //                {
+    //                    _movePoint.position = transform.position + new Vector3(_x * _moveDistance, 0, 0);
+    //                    if (enableMoveCounter) _moveCounter.Move();
+    //                }
+
+    //            }
+    //            else if (Mathf.Abs(_y) == 1)
+    //            {
+    //                // There need to play sound
+    //                // AudioManager.Instance.Play("Jump");
+
+    //                _animator.SetBool("Jumping", true);
+    //                _time = 1;
+    //                if (_y > 0)
+    //                {
+    //                    _animator.SetBool(_activeDirection, false);
+    //                    _activeDirection = "Up";
+    //                    _animator.SetBool(_activeDirection, true);
+    //                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 180);
+    //                }
+    //                else
+    //                {
+    //                    _animator.SetBool(_activeDirection, false);
+    //                    _activeDirection = "Down";
+    //                    _animator.SetBool(_activeDirection, true);
+    //                    _tongueTransform.rotation = Quaternion.Euler(0, 0, 0);
+    //                }
+
+    //                // Check if there drowning object
+    //                if (Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _drownLayer) != null &&
+    //                    Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _itemLayer) == null)
+    //                {
+    //                    _movePoint.position = transform.position + new Vector3(0, _y * _moveDistance, 0);
+    //                }
+
+    //                // Check if there no tile collider
+    //                else if (Physics2D.OverlapCircle(transform.position + new Vector3(0, _y * _moveDistance, 0), .2f, _whatStopsMovement) == null)
+    //                {
+    //                    _movePoint.position = transform.position + new Vector3(0, _y * _moveDistance, 0);
+    //                    if (enableMoveCounter) _moveCounter.Move();
+    //                }
+
+    //            }
+    //        }
+    //    }
+
+    //    // Shooting mode
+    //    else
+    //    {
+    //        _animator.SetBool("Shooting", true);
+    //        float _x = _playerInput.GetMovement().x;
+    //        float _y = _playerInput.GetMovement().y;
+    //        if (_y < 0) _tongueSortingOrder = 11;
+    //        else if (_y != 0 || _x != 0) _tongueSortingOrder = 9;
+
+    //        if (_playerInput.GetShoot() && !_tongue.IsRunning)
+    //        {
+    //            if (enableMoveCounter) _moveCounter.Move();
+
+    //            _tongueTransform.gameObject.SetActive(true);
+    //            _tongueSpriteRenderer.sortingOrder = _tongueSortingOrder;
+    //            _tongue.StartShootTongue();
+
+    //            _playerInput.SetFreeze(true);
+    //            _playerInput.SetShoot(false);
+    //        }
+
+    //        _tongueTransform.gameObject.SetActive(_tongue.IsRunning);
+    //        _playerInput.SetFreeze(_tongue.IsRunning);
+    //        if (_tongue.IsRunning)
+    //        {
+    //            _moveSpeed = _tongue.Speed;
+    //        }
+    //        else
+    //        {
+    //            _moveSpeed = _basicSpeed;
+    //        }
+
+
+    //        if (Mathf.Abs(_x) == 1)
+    //        {
+    //            // There need to play shooting sound
+    //            // AudioManager.Instance.Play("shoot")
+
+    //            if (_x > 0)
+    //            {
+    //                _animator.SetBool(_activeDirection, false);
+    //                _activeDirection = "Right";
+    //                _animator.SetBool(_activeDirection, true);
+    //                _tongueTransform.rotation = Quaternion.Euler(0, 0, 90);
+    //            }
+    //            else
+    //            {
+    //                _animator.SetBool(_activeDirection, false);
+    //                _activeDirection = "Left";
+    //                _animator.SetBool(_activeDirection, true);
+    //                _tongueTransform.rotation = Quaternion.Euler(0, 0, 270);
+    //            }
+    //        }
+
+    //        else if (Mathf.Abs(_y) == 1)
+    //        {
+    //            // There need to play shooting sound
+    //            // AudioManager.Instance.Play("Shoot");
+
+    //            if (_y > 0)
+    //            {
+    //                _animator.SetBool(_activeDirection, false);
+    //                _activeDirection = "Up";
+    //                _animator.SetBool(_activeDirection, true);
+    //                _tongueTransform.rotation = Quaternion.Euler(0, 0, 180);
+    //            }
+    //            else
+    //            {
+    //                _animator.SetBool(_activeDirection, false);
+    //                _activeDirection = "Down";
+    //                _animator.SetBool(_activeDirection, true);
+    //                _tongueTransform.rotation = Quaternion.Euler(0, 0, 0);
+    //            }
+    //        }
+            
+    //    }
+    //}
+
+    enum FrogStates
+    {
+        Idle,
+        Moving,
+        ShootingMode,
+        shooting
     }
 }
